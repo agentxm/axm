79ac0f89d8f8209e263f215f571493b59aafb684
Craig Smitham <craig@agentxm.ai>
2026-02-19 15:29:18 -0800

    refactor(cli): remove agent and extension icon assets

--- packages/cli/src/extensions/index.ts
+++ packages/cli/src/extensions/index.ts
@@ -1,3 +1,2 @@
 export type { ExtensionTypePlural, Fqn } from "./fqn.js";
 export { formatFqn, parseFqn, parseFqnOrThrow } from "./fqn.js";
-export { EXTENSION_TYPE_ICON_PATHS, getExtensionTypeIconPath } from "./icons.js";
